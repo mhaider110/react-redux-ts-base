3d08dd512cc7dd2ddf5710931bd1e463c26048c1
mhaider110 <haider.haider168@gmail.com>
2021-10-27 15:35:45 +0200

    using-redux

--- src/components/home/Home.tsx
+++ src/components/home/Home.tsx
@@ -1,11 +1,27 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useDispatch, useSelector } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import { actionCreators, AppDataState } from '../../state';
 import './home-styles.scss';
 export default function Home() {
+
+  const dispatch = useDispatch();
+  const { saveAppData } = bindActionCreators(actionCreators, dispatch);
+  const stateData = useSelector((state: AppDataState) => state.appData);
+
+  useEffect(() => {
+    const appData = {
+      userName: 'TestUser',
+      userEmail: 'TestEmail'
+    }
+    saveAppData(appData);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [])
 
   return (
     <div className="hw-container full-max-width fixed-padding-container">
       <h2 className="hw-block--mt-larger">
-        <span className="text-bold">{"Hello"} </span>
+        <span className="text-bold">{"Hello "} {stateData.userName } </span>
       </h2>
     </div>
   )
